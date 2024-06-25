cb9b30e112632d4ae2f4c6b1eb783f39c30fa143
Patrick Fisher <patrick.fisher9999@gmail.com>
2024-06-25 20:48:04 +0000

    Get iOS build working, pull metadata from api, initialize MediaElement in code behind

--- app/TorreJams/TorreJams/MainPage.xaml.cs
+++ app/TorreJams/TorreJams/MainPage.xaml.cs
@@ -1,32 +1,110 @@
-﻿namespace TorreJams
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using CommunityToolkit.Maui.Views;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace TorreJams
 {
+    public partial class MediaMetaData
+    {
+        [JsonProperty("title")]
+        public required string Title { get; set; }
+
+        [JsonProperty("artist")]
+        public required string Artist { get; set; }
+
+        [JsonProperty("album")]
+        public required string Album { get; set; }
+
+        [JsonProperty("artwork")]
+        public required Artwork[] Artwork { get; set; }
+    }
+
+    public partial class Artwork
+    {
+        [JsonProperty("src")]
+        public required Uri Src { get; set; }
+
+        [JsonProperty("sizes")]
+        public required string Sizes { get; set; }
+
+        [JsonProperty("type")]
+        public required string Type { get; set; }
+    }
+
+    public partial class MediaMetaData
+    {
+        public static MediaMetaData FromJson(string json) => JsonConvert.DeserializeObject<MediaMetaData>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this MediaMetaData self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new()
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        readonly string BaseURL = $"http://192.168.1.242:8000";
 
         public MainPage()
         {
             InitializeComponent();
-            DownloadFileAsync("http://192.168.0.3:8000/stream/Sense.mp3");
         }
 
-        private async Task DownloadFileAsync(string url)
+        protected override async void OnAppearing()
+        {
+            await GetMusicAndMetadataAsync("Sense.mp3");
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private async Task GetMusicAndMetadataAsync(string songFile)
         {
             try
             {
                 using var httpClient = new HttpClient();
-                var response = await httpClient.GetAsync(url);
+                var response = await httpClient.GetAsync($"{BaseURL}/media_metadata/{songFile}");
                 response.EnsureSuccessStatusCode();
 
-                var fileName = "downloaded_file.mp3"; // You can extract this from the URL or response headers
-                var filePath = Path.Combine(FileSystem.CacheDirectory, fileName);
-
-                using (var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                string? responseStr = await response.Content.ReadAsStringAsync();
+                if (responseStr != null)
                 {
-                    await response.Content.CopyToAsync(fs);
-                }
+                    string trackURL = $"{BaseURL}/stream/{songFile}";
+                    var mediaMetadata = MediaMetaData.FromJson(responseStr);
 
-                await DisplayAlert("Success", $"File downloaded to {filePath}", "OK");
+                    TestMediaElement.ShouldShowPlaybackControls = true;
+                    TestMediaElement.MetadataArtist = mediaMetadata.Artist;
+                    TestMediaElement.MetadataTitle = mediaMetadata.Title;
+                    TestMediaElement.MetadataArtworkUrl = $"{BaseURL}/{mediaMetadata.Artwork[0].Src}";
+                    TestMediaElement.Source = MediaSource.FromUri(trackURL);
+
+                    await DisplayAlert("Success", $"{responseStr}", "OK");
+                }
             }
             catch (Exception ex)
             {
