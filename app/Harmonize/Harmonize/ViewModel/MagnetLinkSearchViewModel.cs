0278aae810c15a909ebd72b0130000fbfa44fe94
John Yenter-Briars <johnyenterbriars@protonmail.com>
2024-10-05 17:17:47 -0500

    initial page for search

--- app/Harmonize/Harmonize/ViewModel/MagnetLinkSearchViewModel.cs
+++ app/Harmonize/Harmonize/ViewModel/MagnetLinkSearchViewModel.cs
@@ -14,19 +14,57 @@ public class MagnetLinkSearchViewModel(
     FailsafeService failsafeService
 ) : BaseViewModel(mediaManager, preferenceManager, failsafeService)
 {
+    #region Bindings
     private string? searchQuery;
     public string? SearchQuery
     {
         get => searchQuery;
         set => SetProperty(ref searchQuery, value);
     }
+    private bool piratebayChecked = true;
+    private bool xt1337Checked;
+    public bool PiratebayChecked
+    {
+        get => piratebayChecked;
+        set
+        {
+            if (piratebayChecked != value)
+            {
+                piratebayChecked = value;
+                OnPropertyChanged();
 
+                if (piratebayChecked)
+                {
+                    Xt1337Checked = false;
+                }
+            }
+        }
+    }
+
+    public bool Xt1337Checked
+    {
+        get => xt1337Checked;
+        set
+        {
+            if (xt1337Checked != value)
+            {
+                xt1337Checked = value;
+                OnPropertyChanged();
+
+                if (xt1337Checked)
+                {
+                    PiratebayChecked = false;
+                }
+            }
+        }
+    }
     private ObservableCollection<MagnetLinkSearchResult> searchResults = [];
     public ObservableCollection<MagnetLinkSearchResult> SearchResults
     {
         get => searchResults;
         set => SetProperty(ref searchResults, value);
     }
+    #endregion
 
     public ICommand SearchCommand => new Command<string>(async (query) =>
     {
@@ -35,13 +73,21 @@ public class MagnetLinkSearchViewModel(
 
         var (results, success) = await failsafeService.Fallback(async () =>
         {
-            return await harmonizeClient.GetPiratebaySearchResults(query);
+            if (PiratebayChecked)
+            {
+                return await harmonizeClient.GetPiratebaySearchResults(query);
+            }
+            else if (Xt1337Checked)
+            {
+                return await harmonizeClient.GetXT1337SearchResults(query);
+            }
+            else return null;
         }, null);
 
         if (success)
         {
             SearchResults.Clear();
-            foreach (var video in results.Value)
+            foreach (var video in results?.Value ?? [])
             {
                 SearchResults.Add(video);
             }
