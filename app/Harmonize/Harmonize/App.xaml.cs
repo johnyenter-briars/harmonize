82fabae6f76aef6880bd8622740e8756b82e21d3
John Yenter-Briars <johnyenterbriars@protonmail.com>
2024-11-02 08:37:58 -0500

    succesful deploy! + global error handling

--- app/Harmonize/Harmonize/App.xaml.cs
+++ app/Harmonize/Harmonize/App.xaml.cs
@@ -1,14 +1,28 @@
 ﻿using Harmonize.Service;
+using Microsoft.Extensions.Logging;
+using System.Runtime.ExceptionServices;
 
-namespace Harmonize
+namespace Harmonize;
+
+public partial class App : Application
 {
-    public partial class App : Application
-    {
-        public App(PreferenceManager preferenceManager)
-        {
-            InitializeComponent();
+    private readonly ILogger<App> logger;
 
-            MainPage = new AppShell(preferenceManager);
-        }
+    public App(
+        ILogger<App> logger,
+        PreferenceManager preferenceManager
+        )
+    {
+        InitializeComponent();
+
+        AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+
+        MainPage = new AppShell(preferenceManager);
+        this.logger = logger;
+    }
+    //https://stackoverflow.com/questions/72455467/how-do-you-create-a-global-error-handling-for-a-maui-app
+    private void CurrentDomain_FirstChanceException(object sender, FirstChanceExceptionEventArgs e)
+    {
+         logger.LogError($"***** Handling Unhandled Exception *****: {e.Exception.Message}");
     }
 }
