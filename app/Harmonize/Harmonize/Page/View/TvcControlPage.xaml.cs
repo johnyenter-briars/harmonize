e16381ca9b5c352878e1310dd41e0c884d3accc5
John Yenter-Briars <johnyenterbriars@protonmail.com>
2026-01-03 09:42:00 -0600

    hold for volume

--- app/Harmonize/Harmonize/Page/View/TvcControlPage.xaml.cs
+++ app/Harmonize/Harmonize/Page/View/TvcControlPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using AlohaKit.Animations;
 using Harmonize.Kodi;
 using Harmonize.Service;
+using Harmonize.TVC;
 using Harmonize.ViewModel;
 using Microsoft.Extensions.Logging;
 
@@ -12,12 +13,14 @@ public partial class TvcControlPage : BasePage<TvcControlViewModel>
     readonly ILogger logger;
     private readonly MediaManager mediaManager;
     private readonly KodiClient kodiClient;
+    private readonly TvcClient tvcClient;
 
     public TvcControlPage(
         TvcControlViewModel viewModel,
         ILogger<HomePage> logger,
         MediaManager mediaManager,
-        KodiClient kodiClient
+        KodiClient kodiClient,
+        TvcClient tvcClient
         ) : base(viewModel)
     {
         InitializeComponent();
@@ -25,28 +28,7 @@ public partial class TvcControlPage : BasePage<TvcControlViewModel>
         this.logger = logger;
         this.mediaManager = mediaManager;
         this.kodiClient = kodiClient;
-    }
-    private async void VolumeSlider_Changed(object sender, ValueChangedEventArgs e)
-    {
-        var slider = (Slider)sender;
-        double distanceFromMin = (slider.Value - slider.Minimum);
-        double sliderRange = (slider.Maximum - slider.Minimum);
-        double sliderPercent = 100 * (distanceFromMin / sliderRange);
-
-        int sliderPercentAsint = (int)sliderPercent;
-
-        await kodiClient.SetVolumeAsync(sliderPercentAsint);
-    }
-    private async void SeekSlider_Changed(object sender, ValueChangedEventArgs e)
-    {
-        var slider = (Slider)sender;
-        double distanceFromMin = (slider.Value - slider.Minimum);
-        double sliderRange = (slider.Maximum - slider.Minimum);
-        double sliderPercent = 100 * (distanceFromMin / sliderRange);
-
-        int sliderPercentAsint = (int)sliderPercent;
-
-        await kodiClient.SeekPlayerAsync(sliderPercentAsint);
+        this.tvcClient = tvcClient;
     }
     private void ScaleButton(object sender, EventArgs e)
     {
@@ -60,5 +42,58 @@ public partial class TvcControlPage : BasePage<TvcControlViewModel>
               }));
         }
     }
+    private CancellationTokenSource? _volumeCts;
+    private async Task StartVolumeLoop(
+    Func<Task> action,
+    int initialDelayMs = 0,
+    int repeatDelayMs = 5)
+    {
+        _volumeCts?.Cancel();
+        _volumeCts?.Dispose();
 
+        _volumeCts = new CancellationTokenSource();
+        var token = _volumeCts.Token;
+
+        try
+        {
+            if (initialDelayMs > 0)
+                await Task.Delay(initialDelayMs, token);
+
+            await action();
+
+            // Repeat while held
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay(repeatDelayMs, token);
+                await action();
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            // Expected when released
+        }
+    }
+    private async void VolumeUp_Pressed(object sender, EventArgs e)
+    {
+        await StartVolumeLoop(() => tvcClient.VolumeUp());
+    }
+
+    private async void VolumeDown_Pressed(object sender, EventArgs e)
+    {
+        await StartVolumeLoop(() => tvcClient.VolumeDown());
+    }
+    private void Volume_Released(object sender, EventArgs e)
+    {
+        _volumeCts?.Cancel();
+        _volumeCts?.Dispose();
+        _volumeCts = null;
+    }
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        _volumeCts?.Cancel();
+        _volumeCts?.Dispose();
+        _volumeCts = null;
+    }
 }
